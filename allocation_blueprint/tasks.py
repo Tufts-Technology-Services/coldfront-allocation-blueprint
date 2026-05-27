0946178e5d5051da6e591526801ab6fec70c0708
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-05-27 14:51:50 -0400

    allow blank allocation attribute blueprint value

--- allocation_blueprint/tasks.py
+++ allocation_blueprint/tasks.py
@@ -16,10 +16,15 @@ def apply_blueprint(allocation_id):
         for attribute_blueprint in attribute_blueprints:
             # don't overwrite existing attributes of the same type for the allocation, only create if it doesn't already exist
             if not AllocationAttribute.objects.filter(allocation=allocation, allocation_attribute_type=attribute_blueprint.allocation_attribute_type).exists():
-                AllocationAttribute.objects.create(
-                    allocation_attribute_type=attribute_blueprint.allocation_attribute_type,
-                    value=get_attribute_value(allocation.id, attribute_blueprint.value),
-                    allocation=allocation)
+                if attribute_blueprint.value:
+                    AllocationAttribute.objects.create(
+                        allocation_attribute_type=attribute_blueprint.allocation_attribute_type,
+                        value=get_attribute_value(allocation_id, attribute_blueprint.value),
+                        allocation=allocation)
+                else:
+                    AllocationAttribute.objects.create(
+                        allocation_attribute_type=attribute_blueprint.allocation_attribute_type,
+                        allocation=allocation)
         logger.info(f"Created allocation attributes for allocation {allocation.id} based on blueprint {blueprint.id}")
     else:
         logger.debug(f"No allocation blueprint found for resource {allocation.get_parent_resource.name}. No attributes created for allocation {allocation.id}.")
